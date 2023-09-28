bf5cc786bc102a89b76b3c304e82370759a2492f
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-27 22:49:32 -0300

    Update app.vue

--- beyond-app/src/App.vue
+++ beyond-app/src/App.vue
@@ -2,42 +2,37 @@
   <v-app>
     <v-main>
       <v-app-bar color="green">Roberto</v-app-bar>
-      <div v-for="(messages, index) in messages" :key="index">
-
-        <v-card v-if="messages.nome != 'ele'" outlined class="ma-3 teal accent-4">
-          <v-card-subtitle class="font-weight-bold">{{ messages.nome }}</v-card-subtitle>
-          <v-card-text>{{messages.text}}</v-card-text>
+      <div>
+        <v-card v-for="(message, index) in messages" :key="index" :class="message.nome !== 'Eu' ? 'teal accent-4' : 'purple accent-4'" outlined class="ma-3">
+          <v-card-subtitle class="font-weight-bold">{{ message.nome }}</v-card-subtitle>
+          <v-card-text>{{ message.text }}</v-card-text>
         </v-card>
-      
-        
-        
       </div>
     </v-main>
     <v-app-bar app fixed bottom color="#424242">
-      <v-text-field solo hide-details label="Enviar mensagem"></v-text-field>
+      <v-text-field v-model=""solo hide-details label="Enviar mensagem..."></v-text-field>
       <v-btn icon color="blue">
         <v-icon>mdi-send</v-icon>
       </v-btn>
     </v-app-bar>
   </v-app>
 </template>
+
 <script>
-export default{
-  data(){
-    return{
-      messages:[
+export default {
+  data() {
+    return {
+      messages: [
         {
+          textInput: "",
           nome: "Roberto",
           text: "Olá"
         },
         {
-        nome: "Roberto",
+          nome: "Roberto",
           text: "Tudo bem"
         },
-        {
-        nome: "ele",
-          text: "Sou trouxa"
-        },
+       
       ]
     }
   }
